67971bb6fa346bd472a3380e5fbde4cc98e94c82
RepIngSE <saragabriela2604@gmail.com>
2024-04-05 00:59:29 -0500

    Cambios_Finales_Mensajes

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1,36 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\chat;
-use App\Http\Controllers\AuthController; 
-use Dotenv\Exception\ValidationExcept;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; 
 use Illuminate\Support\Facades\DB; 
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
+//Funciones de la pantalla Chat y Mnesajes 
 class ChatController extends Controller{
-    public function getChats(){
-        try {
-            // Verificar si el usuario está autenticado
-            if (!Auth::check()) {
-                // Si el usuario no está autenticado, enviar una respuesta 401
-                return response()->json(['error' => 'No autorizado'], 401);
-            }
 
-            // Intentar obtener al usuario autenticado
-            $user = Auth::userOrFail();
-
-            $datos = DB::table('chats') ->get(); 
-            return response()-> json($datos); 
-        }catch (\Exception $e) {
-            // Si hay un error interno del servidor, enviar una respuesta 500 con información del error
-            return response()->json(['error' => 'Error interno del servidor', 'message' => $e->getMessage()], 500);
-        }
-    }
-
+    //Función para traer todos los mensajes de un chat especifico 
+    //(recibe un parámetro en el header llamado id_chat)
     public function getMensajesChat(Request $request){
         
         try {
@@ -40,65 +19,63 @@ class ChatController extends Controller{
                 return response()->json(['error' => 'No autorizado'], 401);
             }
 
-            // Intentar obtener al usuario autenticado
+            // Obtener al usuario autenticado
             $user = Auth::userOrFail();
             
-            // Obtener los valores de los parámetros de consulta 'remitente' y 'destinatario' enviados en la solicitud
-            $usuario1 = $request->query('usuario1');
-            $usuario2 = $request->query('usuario2');
-            $tipo = $request->query('chat');
+            // Obtener a partir del front el id del chat, para traer todos los mensajes
+            $id_chat = $request->header('id_chat');
         
-            // Ejecutar la consulta aplicando los filtros según los parámetros proporcionados
-            $chat = DB::table('chats')
-                        ->select('id')
-                        ->where(function ($query) use ($usuario1, $usuario2, $tipo) {
-                            $query->where('usuario1_id', $usuario1)
-                                ->where('usuario2_id', $usuario2)
-                                ->where('tipoChat', $tipo)
-                                ->orWhere(function ($query) use ($usuario2, $usuario1, $tipo) {
-                                    $query->where('usuario2_id', $usuario1)
-                                            ->where('usuario1_id', $usuario2)
-                                            ->where('tipoChat', $tipo); 
-                                });
-                        })
+            // Ejecutar la consulta para conectar con la tabla chats y de esa forma traer los mensajes que corresponden al id del chat
+            $mensajes = DB::table('mensajes')
+                        ->where('chat', $id_chat)
+                        ->where ('activo', 1)
                         ->get();
 
-            $mensajes = DB::table('mensajes')
-                        ->where('chat', $chat[0]->id)
-                        ->get();
+            // Utilizar la función map para identificar si el usuario autentificado envio el mensaje
+            $mensajes = $mensajes->map(function ($mensaje) use ($user) {
+
+                // Marcar el mensaje como verdadero si el usuario autentificado fue quien envio el mensaje 
+                $mensaje->remitente_id = ($mensaje->remitente_id == $user->id);
+                return $mensaje;
+            });
+
             // Retornar los datos filtrados
             return response()->json($mensajes, 200);
+
         } catch (\Exception $e) {
+
             // Si hay un error interno del servidor, enviar una respuesta 500 con información del error
             return response()->json(['error' => 'Error interno del servidor', 'message' => $e->getMessage()], 500);
         }
     }
     
+    //Función para traer los chats dependiendo de su tipo (Amistad o Relación)
+    //(recibe un parámetro en el header llamado tipo_chat)
     public function getChat(Request $request){
 
         try {
             // Verificar si el usuario está autenticado
             if (!Auth::check()) {
+
                 // Si el usuario no está autenticado, enviar una respuesta 401
                 return response()->json(['error' => 'No autorizado'], 401);
             }
 
-            // Intentar obtener al usuario autenticado
+            // Obtener al usuario autenticado
             $user = Auth::userOrFail();
 
             // Obtener los valores de los parámetros de consulta 'remitente' y 'destinatario' enviados en la solicitud
-            $usuario1 = $request->query('usuario1');
-            $tipo = $request->query('chat');
+            $tipo_chat = $request->header('tipo_chat');
             
             // Ejecutar la consulta aplicando los filtros según los parámetros proporcionados
             $chat = DB::table('chats')
             ->select('id')
-                ->where(function ($query) use ($usuario1, $tipo) {
-                    $query->where('chats.usuario1_id', $usuario1)
-                        ->where('chats.tipoChat', $tipo)
-                        ->orWhere(function ($query) use ($usuario1, $tipo) {
-                            $query->where('chats.usuario2_id', $usuario1)
-                                ->where('chats.tipoChat', $tipo);
+                ->where(function ($query) use ($user, $tipo_chat) {
+                    $query->where('chats.usuario1_id', $user -> id)
+                        ->where('chats.tipoChat', $tipo_chat)
+                        ->orWhere(function ($query) use ($user, $tipo_chat) {
+                            $query->where('chats.usuario2_id', $user-> id)
+                                ->where('chats.tipoChat', $tipo_chat);
                         });
                     })
                 ->get();
@@ -106,13 +83,15 @@ class ChatController extends Controller{
                 $mensajes = [];
                 foreach ($chat as $chats) {
                     $ultimoMensaje = DB::table('mensajes')
-                        ->select('mensajes.chat','mensajes.id','mensajes.mensaje', 'mensajes.visto', DB::raw('CASE WHEN chats.usuario1_id = ' . $usuario1 . ' THEN u2.nombre ELSE u1.nombre END AS nombre_usuario'))
+                        ->select('mensajes.chat','mensajes.id','mensajes.mensaje', 'mensajes.visto', DB::raw('CASE WHEN chats.usuario1_id = ' . $user->id. ' THEN u2.nombre ELSE u1.nombre END AS nombre_usuario'))
                         ->join('chats', 'mensajes.chat', '=', 'chats.id')
                         ->join('users as u1', 'chats.usuario1_id', '=', 'u1.id')
                         ->join('users as u2', 'chats.usuario2_id', '=', 'u2.id')
                         ->where('mensajes.chat', $chats->id)
+                        ->where('mensajes.activo', 1)
                         ->orderBy('mensajes.id', 'desc')
-                        ->first(); // Obtener el primer resultado, que será el último mensaje debido al orden descendente
+                        // Obtener el primer resultado, que será el último mensaje debido al orden descendente
+                        ->first(); 
                 
                     // Agregar el último mensaje de este chat al array de mensajes, si existe
                     if ($ultimoMensaje) {
@@ -122,26 +101,31 @@ class ChatController extends Controller{
                 
                 // Retornar los datos filtrados
                 return response()->json($mensajes);
+
         } catch (\Exception $e) {
+
             // Si hay un error interno del servidor, enviar una respuesta 500 con información del error
             return response()->json(['error' => 'Error interno del servidor', 'message' => $e->getMessage()], 500);
         }
     }
 
+    //Función que nos trae todos los chats que tenemos, tanto amistad como relación
     public function getAllChat(){
 
         try {
             // Verificar si el usuario está autenticado
             if (!Auth::check()) {
+
                 // Si el usuario no está autenticado, enviar una respuesta 401
                 return response()->json(['error' => 'No autorizado'], 401);
             }
 
-            // Intentar obtener al usuario autenticado
+            // Obtener al usuario autenticado
             $user = Auth::userOrFail();
 
-            // Ejecutar la consulta aplicando los filtros según los parámetros proporcionados
+            // Consulta que nos devuelve todos los chats del usuario autenticado
             $chat = DB::table('chats')
+
             ->select('id')
                 ->where(function ($query) use ($user) {
                     $query->where('chats.usuario1_id', $user->id)
@@ -159,12 +143,14 @@ class ChatController extends Controller{
                         ->join('users as u1', 'chats.usuario1_id', '=', 'u1.id')
                         ->join('users as u2', 'chats.usuario2_id', '=', 'u2.id')
                         ->where('mensajes.chat', $chats->id)
+                        ->where('mensajes.activo', 1)
                         ->orderBy('mensajes.id', 'desc')
-                        ->first(); // Obtener el primer resultado, que será el último mensaje debido al orden descendente
+                        // Obtener el primer resultado, que será el último mensaje debido al orden descendente
+                        ->first(); 
                 
                     // Verificar si el remitente del mensaje es el usuario autenticado
-                    // Verificar si se encontró un mensaje
                     if ($ultimoMensaje) {
+
                         // Agregar el mensaje al array de mensajes con el remitente indicado
                         $mensajes[] = [
                             "chat"=>$ultimoMensaje->chat,
@@ -172,21 +158,23 @@ class ChatController extends Controller{
                             "mensaje"=>$ultimoMensaje->mensaje,
                             "visto"=>$ultimoMensaje->visto,
                             "remitente"=>$ultimoMensaje->remitente_id == $user->id ? True : False,
-                            "nombre_usuario"=>$ultimoMensaje->nombre_usuario
+                            "nombre_destinatario"=>$ultimoMensaje->nombre_usuario
                         ];
                     }
                 }
-                
             
             // Retornar los datos filtrados
             return response()->json($mensajes);
 
         }catch (\Exception $e) {
+
             // Si hay un error interno del servidor, enviar una respuesta 500 con información del error
             return response()->json(['error' => 'Error interno del servidor', 'message' => $e->getMessage()], 500);
         }
     }
     
+    //Función para enviar mensajes (guardarlos en la base de datos)
+    //(Recibe el parametro mensaje y id_chat para identificar con quien es la conversación)
     public function sendMensajes(Request $request){
 
         try {
@@ -199,22 +187,22 @@ class ChatController extends Controller{
             // Intentar obtener al usuario autenticado
             $user = Auth::userOrFail();
 
-            // Obtener los valores de los parámetros de consulta 'remitente' y 'destinatario' enviados en la solicitud
-            $usuario1 = $request->header('usuario1');
+            // Guardamos los parametros enviados en el header
             $mensaje = $request->header('mensaje');
-            $chat = $request->header('chat');
-
+            $id_chat = $request->header('id_chat');
+            
+            // Sentencia de insersión de mensajes
             DB::table('mensajes')->insert([
-                'remitente_id'=>$usuario1, 
+                'remitente_id'=>$user -> id, 
                 'mensaje'=>$mensaje,
-                'chat'=>$chat
+                'chat'=>$id_chat
             ]); 
         
         } catch (\Exception $e) {
-                // Si hay un error interno del servidor, enviar una respuesta 500 con información del error
-                return response()->json(['error' => 'Error interno del servidor', 'message' => $e->getMessage()], 500);
-            }
+
+            // Si hay un error interno del servidor, enviar una respuesta 500 con información del error
+            return response()->json(['error' => 'Error interno del servidor', 'message' => $e->getMessage()], 500);
+        }
     }
-    
 }
 
--- routes/api.php
+++ routes/api.php
@@ -21,25 +21,45 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group([
 
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'middleware' => 'api'
 
 ], function ($router) {
     //rutas para acceder desde y realizar acciones necesarias para el login 
     //se loguea y crea el token del user
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('/auth/login', 'App\Http\Controllers\AuthController@login');
     //se desloguea y vence el token 
-    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::get('/auth/logout', 'App\Http\Controllers\AuthController@logout');
     //refresca el token
-    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::get('/auth/refresh', 'App\Http\Controllers\AuthController@refresh');
     //trae toda la informacion del user
-    Route::post('datosUser', 'App\Http\Controllers\AuthController@datosUser');
+    Route::get('/auth/datosUser', 'App\Http\Controllers\AuthController@datosUser');
     //permite crear un nuevo usuario
-    Route::post('registro', 'App\Http\Controllers\AuthController@register');
-    
+    Route::post('/auth/registro', 'App\Http\Controllers\AuthController@register');
+
+    //cambio de path para que se centre en el perfil
+    Route::post('/perfil/upsertPerfil','App\Http\Controllers\PerfilesController@upsertPerfil');
+    Route::put('/perfil/actualizarPerfil','App\Http\Controllers\PerfilesController@actualizarPerfil');
+
+    //cambio de path para traer los usuarios de amistad o ambos  searchRelacion
+    Route::get('/search/friendship','App\Http\Controllers\PerfilesController@searchFriendship');
+    Route::get('/search/relationship','App\Http\Controllers\PerfilesController@searchRelationship');
+
 });
 
-Route::get('/mensajeChat',[ChatController::class, 'getMensajesChat']);
-Route::get('/mensajesFinal',[ChatController::class, 'getChat']); 
-Route::get('/mensajesAll',[ChatController::class, 'getAllChat']); 
-Route::post('/envioMensajes',[ChatController::class, 'sendMensajes']); 
+// Grupo de rutas para Mensajes
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'Mensajes'
+
+], function ($router) {
+
+    // Api para traer todos los mensajes de un chat especifico 
+    Route::get('/MensajeChat',[ChatController::class, 'getMensajesChat']);
+    // Api para traer los chats dependiendo de su tipo (Amistad o Relación)
+    Route::get('/Chats',[ChatController::class, 'getChat']);
+    // Api para traer todos los chats que tenemos, tanto amistad como relación
+    Route::get('/ChatsAll',[ChatController::class, 'getAllChat']); 
+    // Api para enviar mensajes (guardarlos en la base de datos)
+    Route::post('/EnvioMensajes',[ChatController::class, 'sendMensajes']); 
+});
